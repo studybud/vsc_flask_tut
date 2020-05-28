0e19aeecfb2efee4486c0c8ceb991f09d5fbe437
studybud <study.buddy.0907@gmail.com>
2020-05-28 08:35:28 +0200

    initial tut commit

---
+++ flask_tut_app/__init__.py
@@ -0,0 +1,4 @@
+import flask
+
+print("loading __init__.py")
+app = flask.Flask(__name__)
---
+++ helper_urls.py
@@ -0,0 +1,15 @@
+# basics
+
+test_urls = [
+"http://127.0.0.1:5000/home",
+"http://127.0.0.1:5000/home/",
+"http://127.0.0.1:5000/hello",
+"http://127.0.0.1:5000/hello/",
+"http://127.0.0.1:5000//api/data",
+"http://127.0.0.1:5000/hello/kitty",
+"http://127.0.0.1:5000/hello/kitty/",
+"http://127.0.0.1:5000/about",
+"http://127.0.0.1:5000/about/",
+"http://127.0.0.1:5000/contact",
+"http://127.0.0.1:5000/contact/"]
+
